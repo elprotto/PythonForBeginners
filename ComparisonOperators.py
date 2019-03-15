fc3fe5d503aaf3d868cdb3814a7b7c37bfd2134a
Alexander <elprotto@gmail.com>
2019-03-15 18:23:05 -0500

    Create ComparisonOperators.py
    Repaso de operadores de comparación.

---
+++ ComparisonOperators.py
@@ -0,0 +1,13 @@
+# Comparison Operators
+# Siempre retornan un valor booleano
+
+print(10 > 3)
+print(10 >= 2)
+print(10 < 20)
+print(10 == 10)
+print(10 == "10")
+print(10 != "10")
+print("bag" > "apple")
+print("bag" == "BAG")
+print(ord("b"))
+print(ord("B"))
