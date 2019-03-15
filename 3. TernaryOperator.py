e07037d5872eb8dfdf71fa3b89f175603254b3ad
Alexander <elprotto@gmail.com>
2019-03-15 18:47:23 -0500

    Create 3. TernaryOperator.py
    Ejemplo de operador ternario

---
+++ 3. TernaryOperator.py
@@ -0,0 +1,11 @@
+edad = 15
+
+if edad > 18:
+    mensaje = "Vete de la casa"
+else:
+    mensaje = "Aun eres un mantenido/mantenida"
+
+mensaje2 = "Libre" if edad >= 18 else "No eres libre"
+
+print(mensaje)
+print(mensaje2)
