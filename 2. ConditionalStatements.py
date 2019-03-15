9d9d8f9e6b83ed987bc7192bc92364236c1c8e89
Alexander <elprotto@gmail.com>
2019-03-15 18:39:42 -0500

    Create 2. ConditionalStatements.py
    Ejemplos de control de flujo.

---
+++ 2. ConditionalStatements.py
@@ -0,0 +1,50 @@
+
+# Condicionales
+temperatura = 31
+
+if temperatura > 30:
+    print("Está muy caliente")
+else:
+    print("Está perfecto")
+
+
+condicion = True
+
+if condicion:
+    print("La condicion es verdadera")
+
+if True:
+    print("La condicion es verdadera")
+
+nuevaCondicion = 1
+nuevaCondicion2 = 0
+nuevaCondicion3 = ""
+nuevaCondicion4 = None
+
+if nuevaCondicion:
+    print("La condicion es verdadera")
+else:
+    print("La condicion es falsa")
+
+if nuevaCondicion2:
+    print("La condicion es verdadera")
+else:
+    print("La condicion es falsa")
+
+if nuevaCondicion3:
+    print("La condicion es verdadera")
+else:
+    print("La condicion es falsa")
+
+if nuevaCondicion4:
+    print("La condicion es verdadera")
+else:
+    print("La condicion es falsa")
+
+
+if temperatura > 30:
+    print("La temp es mayor que 30")
+elif temperatura > 20:
+    print("Estamos en medallo?")
+else:
+    print("La condicion es falsa")
