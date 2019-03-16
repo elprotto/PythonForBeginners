cd06fac7e1af23e1a4bf6709e1328f6643036e0e
Alexander <elprotto@gmail.com>
2019-03-15 19:11:44 -0500

    Create 5. ChainingComparisonOperators.py
    Ejemplos de operadores en cadena

---
+++ 5. ChainingComparisonOperators.py
@@ -0,0 +1,11 @@
+edad = 65
+
+# if edad >= 18 and edad < 45:
+#     print("Si erer mujer es la edad chevere")
+# else:
+#     print("Tambien estas chevere")
+
+if 18 <= edad < 45:
+    print("Si erer mujer es la edad chevere")
+else:
+    print("Tambien estas chevere")
