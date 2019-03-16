d18f768462a7831a932a4f0581c01f9c88c98c33
Alexander <elprotto@gmail.com>
2019-03-15 19:26:12 -0500

    Create 7. ForLoops.py
    ejemplos de for usando range

---
+++ 7. ForLoops.py
@@ -0,0 +1,17 @@
+# print(range(3))
+
+for number in range(4):
+    print(number)
+
+# lista = [1, 2, 3, 4]
+
+# number = 3
+
+# if number in range(4):
+#     print("Esta en la lista")
+
+for number in range(10):
+    print("Intento", number, (number + 1) * ".")
+
+for number in range(1, 20, 3):
+    print("nivel", number, (number + 1) * ".")
